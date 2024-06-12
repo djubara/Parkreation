34792d220cad56e54139210295d905d2661bff55
JustindHolderman <160930788+JustindHolderman@users.noreply.github.com>
2024-06-12 15:30:20 -0500

    Comment, park, user models in place and called within server.js

---
+++ models/index.js
@@ -0,0 +1,4 @@
+const User = require('./users');
+const Comments = require('./comments');
+const Parks = require('./park');
+module.exports = { User, Comments };
---
+++ models/park.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connections');
+
+class Park extends Model {}
+
+Park.init(
+    {
+        id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+        },
+        park_website: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        },
+        park_description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        },
+        park_image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'park',
+    }
+    );
+
+module.exports = Park;
