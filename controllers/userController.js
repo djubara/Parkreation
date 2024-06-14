83b68f95386205f143626f01087cd013cb4b7c90
Dia Jubara <djubara@hotmail.com>
2024-06-14 09:26:54 -0500

    created models

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,5 @@
+const { create } = require("handlebars");
+
 module.exports = {
     async getUserByID(userID) {
         // TODO sequelize query to get user by ID
@@ -8,4 +10,19 @@ module.exports = {
     async createUser(userData) {
         // TODO sequelize query to create a new user
     },  // TODO Modeler to create a new user columns
+    createUser: async (userData) => {
+        try {
+            const newUser = await User.create(userData);
+            return newUser;
+        } catch (err) {
+            console.log(err);
+            return err;
+        }
+    },
+    async deleteUser(userID) {
+        // TODO sequelize query to delete a user
+    },
+    async updateUser(userData) {
+        // TODO sequelize query to update a user
+    },
 };
