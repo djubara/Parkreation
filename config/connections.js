6edbe18f6a538f56ea9df46c03cbcd31b4e2c159
Dia Jubara <djubara@hotmail.com>
2024-06-17 17:46:35 -0500

    connection for render

--- config/connections.js
+++ config/connections.js
@@ -1,11 +1,27 @@
+// require('dotenv').config();
+// const Sequelize = require('sequelize');
+// const sequelize = process.env.DB_URL ? new Sequelize(process.env.DB_URL, { dialect: 'postgres', dialectOption: { decimalNumbers: true } }) : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+//   // use localhost if db url is null
+//   host: 'localhost',
+//   dialect: 'postgres',
+//   dialectOptions: {
+//     decimalNumbers: true,
+//   },
+// });
+// module.exports = sequelize;
 require('dotenv').config();
 const Sequelize = require('sequelize');
-const sequelize = process.env.DB_URL ? new Sequelize(process.env.DB_URL, { dialect: 'postgres', dialectOption: { decimalNumbers: true } }) : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  // use localhost if db url is null
-  host: 'localhost',
-  dialect: 'postgres',
-  dialectOptions: {
-    decimalNumbers: true,
+
+let sequelize
+
+sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_URL ?? 'localhost',
+    dialect: 'postgres'
   },
-});
+);
+
 module.exports = sequelize;
